8d8b4f0d207c4d1cd3a401671e16deadd85e4ed4
diegojfcampos <diegojfcampos@gmail.com>
2023-09-05 21:12:42 +0100

    fix: .env db url

--- utils/db.js
+++ utils/db.js
@@ -3,23 +3,24 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
+  mongoose.set('strictQuery', true);
 
-    if(isConnected){
-        console.log('MongoDB is already connected');
-        return;
-    }
-    try{
-        await mongoose.connect(process.env.MONGODB_URL, {
-            dbName:"chatter_forge",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-        isConnected = true;
-        console.log('MongoDB Connected');
-        
-    }catch(err){
-        console.log(err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "chatter_forge",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
