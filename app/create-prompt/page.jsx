7f57ba493d56732d006a0e48ee25a6f6eaaea1cd
diegojfcampos <diegojfcampos@gmail.com>
2023-09-11 17:52:43 +0100

    fix: importing Form on Page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,26 +2,29 @@
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { Form } from '@components/Form'
+import Form  from '@components/Form'
 
 
 const CreatePrompt = () => {
+
     const [submitting, setSubmitting]= useState(false)
+
     const[post, setPost] = useState({
         prompt: '',
         tag: '',
     });
 
     const createPrompt = async(e) => {}
-  return (
-    <Form
-        type="Create"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={createPrompt}
-    />
-  )
+
+    return (
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
+    )
 }
 
 export default CreatePrompt
