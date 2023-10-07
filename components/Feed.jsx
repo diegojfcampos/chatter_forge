5c7582f2fb6a9469d26595038401d896ba41b85d
diegojfcampos <diegojfcampos@gmail.com>
2023-10-07 01:07:33 +0100

    feat: prompt find populate

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,7 +4,7 @@ import PromptCard from './PromptCard';
 
 const PromptCardList = ({data, handleTagClick}) => {
   return(
-    <div className='mt-6 prompt_layout'>
+    <div className='mt-16 prompt_layout'>
       {
         data.map((post) => {
           <PromptCard 
